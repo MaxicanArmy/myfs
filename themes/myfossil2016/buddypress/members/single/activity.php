e32f55d37ec90ff1f2edbc9e621c55b8ed210cef
Max H Jones <mjones@atmoapps.com>
2018-10-19 16:15:03 -0400

    Removed the Whats New textarea when you're viewing another user's profile as posting there just posts on your own activity stream and default Buddypress does not display a textarea in that situation.

--- themes/myfossil2016/buddypress/members/single/activity.php
+++ themes/myfossil2016/buddypress/members/single/activity.php
@@ -4,34 +4,77 @@
  *
  * @package BuddyPress
  * @subpackage bp-legacy
+ * @version 3.0.0
  */
+
 ?>
 
 <div class="row">
-
-    <div class="item-list-tabs no-ajax" id="subnav" role="navigation">
+    <div class="item-list-tabs no-ajax" id="subnav" aria-label="<?php esc_attr_e( 'Member secondary navigation', 'buddypress' ); ?>" role="navigation">
         <ul class="nav nav-pills" id="nav-sub">
+
             <?php bp_get_options_nav(); ?>
+
+            <li id="activity-filter-select" class="last">
+                <label for="activity-filter-by"><?php _e( 'Show:', 'buddypress' ); ?></label>
+                <select id="activity-filter-by">
+                    <option value="-1"><?php _e( '&mdash; Everything &mdash;', 'buddypress' ); ?></option>
+
+                    <?php bp_activity_show_filters(); ?>
+
+                    <?php
+
+                    /**
+                     * Fires inside the select input for member activity filter options.
+                     *
+                     * @since 1.2.0
+                     */
+                    do_action( 'bp_member_activity_filter_options' ); ?>
+
+                </select>
+            </li>
         </ul>
     </div><!-- .item-list-tabs -->
-
 </div>
-
 <div class="row">
+<?php
 
-    <div class="activity" role="main">
-        <?php
-        do_action( 'bp_before_member_activity_content' );
-	
-		if ( is_user_logged_in() ) {
-		
-			bp_get_template_part( 'activity/post-form' ); 
-		}
-		
-        bp_get_template_part( 'activity/activity-loop' );
-        ?>
+/**
+ * Fires before the display of the member activity post form.
+ *
+ * @since 1.2.0
+ */
+do_action( 'bp_before_member_activity_post_form' ); ?>
+
+<?php
+if ( is_user_logged_in() && bp_is_my_profile() && ( !bp_current_action() || bp_is_current_action( 'just-me' ) ) )
+    bp_get_template_part( 'activity/post-form' );
+
+/**
+ * Fires after the display of the member activity post form.
+ *
+ * @since 1.2.0
+ */
+do_action( 'bp_after_member_activity_post_form' );
+
+/**
+ * Fires before the display of the member activities list.
+ *
+ * @since 1.2.0
+ */
+do_action( 'bp_before_member_activity_content' ); ?>
+
+    <div class="activity" aria-live="polite" aria-atomic="true" aria-relevant="all">
+
+        <?php bp_get_template_part( 'activity/activity-loop' ) ?>
+
     </div><!-- .activity -->
-
 </div>
+<?php
 
-<?php do_action( 'bp_after_member_activity_content' ); ?>
+/**
+ * Fires after the display of the member activities list.
+ *
+ * @since 1.2.0
+ */
+do_action( 'bp_after_member_activity_content' );
